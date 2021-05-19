42af6a7fc64faa50f48a3462713186a3aac13a56
Zhao Yingjie <yingjie.zhao@student-cs.fr>
2021-05-19 17:00:33 +0200

    233

---
+++ src/myVelib/testUI.java
@@ -0,0 +1,19 @@
+package myVelib;
+
+public class testUI {
+	public static void main(final String[] arguments)
+	{
+	   if (arguments.length < 1)
+	   {
+	      System.out.println("\nNo file path/name provided; please provide a file path/name.\n");
+	      System.out.println("\tUSAGE: SeriesExample <filePathAndName> [-v|--verbose]");
+	   }
+	   else
+	   {
+	      final String file = arguments[0];
+	      final String verboseString = arguments.length > 1 ? arguments[1] : "";
+	      final boolean verbose = verboseString.equals("-v") || verboseString.equals("--verbose");
+	      System.out.println("File path/name is '" + file + "' and verbosity is " + verbose);
+	   }
+	}
+}
