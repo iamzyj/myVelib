38460777c45257666b3d00fc28a2d0a21342f45a
Liao Shuai <shuai.liao@student-cs.fr>
2021-05-19 15:12:34 +0200

    time

---
+++ src/myVelib/Time.java
@@ -0,0 +1,20 @@
+package myVelib;
+
+import java.util.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Time extends Date {
+	public Date time;
+	public String format="yyyy-MM-dd HH:mm:ss";
+	public Time() {
+		super();
+		this.time=new Date();
+	}
+	public Time(String s) throws ParseException {
+		super();
+		SimpleDateFormat sdf = new SimpleDateFormat(this.format);
+		this.time=sdf.parse(s);
+	}
+
+}
