a6aff07063e90202d4e62d07b7d7a67cb43cea97
Zhao Yingjie <yingjie.zhao@student-cs.fr>
2021-05-19 14:29:36 +0200

    233

--- src/myVelib/Slot.java
+++ src/myVelib/Slot.java
@@ -4,4 +4,12 @@ public class Slot {
 	public int ID;
 	public boolean free;
 	public Bicycle bicycle;
+	public Slot(int iD, boolean free) {
+		super();
+		ID = iD;
+		this.free = free;
+	}
+	
+	
+	
 }
--- src/myVelib/Station.java
+++ src/myVelib/Station.java
@@ -7,5 +7,22 @@ public class Station {
 	ArrayList<Slot> slot;
 	int ID;
 	boolean Online;
+	
+	
+	
+	
+	public Station(Coordinates co, ArrayList<Slot> slot, int iD, boolean online) {
+		super();
+		Co = co;
+		this.slot = slot;
+		ID = iD;
+		Online = online;
+	}
+	
+	public static ArrayList<Slot> GenerateSlot(int n) {
+		ArrayList<Slot>  slot = new ArrayList<Slot>(n);
+		
+	}
+	
 }
 
