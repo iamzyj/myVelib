ca28ee12ae7d658f10194852b49ea8345e7ebf32
Liao Shuai <shuai.liao@student-cs.fr>
2021-05-21 17:01:02 +0200

    all

--- src/myVelib/VelibSystem.java
+++ src/myVelib/VelibSystem.java
@@ -1,13 +1,17 @@
 package myVelib;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class VelibSystem {
 	ArrayList<Bicycle> bicycles = new ArrayList<Bicycle>();
 	ArrayList<Station> stations =new ArrayList<Station>();
 	ArrayList<User> users=new ArrayList<User>();
+	String name;
+	public VelibSystem(String name) {
+		this.name=name;
+	}
 	public static void main(String args[]) {
-		VelibSystem s= new VelibSystem();
+		VelibSystem s= new VelibSystem("001");
 		
 		for(int i=0;i<10;i++) {
 			int id;
@@ -21,5 +25,28 @@ public class VelibSystem {
 		for (Bicycle b:s.bicycles) {
 		System.out.println(b.getID());
 		}
+	}
+	public void generateStations(int nStations,int nSlots) {
+		HashSet<Coordinates> c=new HashSet<Coordinates>();
+		while(c.size()<nStations) {
+			c.add(new Coordinates());
+		}
+		int i=0;
+		for(Coordinates cs:c) {
+			stations.add(new Station(cs,nSlots,i,true));
+		}
+
+	}
+	public void distributeBikes(int nBikes) {
+		int nMech=(int)(nBikes*0.7);
+		int nElec=nBikes-nMech;
+		
+	}
+	public void setup(String name,int nStations, int nSlots, int nBikes) {
+		VelibSystem s= new VelibSystem(name);
+		s.generateStations(nStations, nSlots); 
+		int nMech=(int)(nBikes*0.7);
+		int nElec=nBikes-nMech;
+		
 	}
 }
