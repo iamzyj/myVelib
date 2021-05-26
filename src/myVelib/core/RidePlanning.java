d80f1de22487425f066114cf6283d12f9f1818d8
Zhao Yingjie <yingjie.zhao@student-cs.fr>
2021-05-26 20:35:31 +0200

    1 2

--- src/myVelib/core/RidePlanning.java
+++ src/myVelib/core/RidePlanning.java
@@ -29,10 +29,73 @@ public class RidePlanning {
 	
 	
 	
+@SuppressWarnings("unused")
+public TreeMap<Double,Station> filter(TreeMap<Double,Station> stations, int flag){
+		
+		HashMap<Double, Station> sortStation = new HashMap<>();
+		
+		
+		if (flag==0)
+			return stations;
+		else if (flag==1) {
+			for (Double key : stations.keySet()) {
+				
+				Station tempStation=stations.get(key);
+				if (tempStation.isPlus==true)
+					stations.remove(key);
+				else
+					break;
+			}
+			
+			return stations;
+		}
+		
+		else if (flag==2) {
+			int cnt=0;
+			double mindis = 0;
+			for (Double key : stations.keySet()) {		
+				Station tempStation=stations.get(key);
+				if (cnt==0)
+				{
+					if (tempStation.isPlus==true){
+						return stations;
+					}
+					else {
+						mindis=key;
+					}
+					cnt++;
+					break;
+				}
+				if (tempStation.isPlus==true) {
+					if (key>1.1*mindis) {
+						return stations;
+					}
+					else {
+						for (int i=0;i<cnt;i++) {
+							for (Double key1 : stations.keySet()) {
+									stations.remove(key1);
+									break;
+							}
+							return stations;
+						}
+					}
+				}
+				cnt++;	
+			}
+			
+			
+			
+		}
+		return stations;
+	}
+	
+	
+	
 	
 	public HashMap<String,Station> Planning(HashMap<Integer,Station> stations, Coordinates start, Coordinates end){
-		
+		int flag;//flag==0 : normal   flag==1 avoid plus   flag==2 prefer plus  flag==3 balance;
 		System.out.println("Would you like to avoid plus station? (yes/no):");
+		
 		System.out.println("Would you like to prefer plus station? (yes/no):");
 		TreeMap<Double, Station> sortedStart =  Sort(stations, start);
 		TreeMap<Double, Station> sortedEnd =	Sort(stations, end);
